ca7febf90cebfa42ea787659a8ae1868784c8cf9
Aleksey Smolenchuk <lxe@lxe.co>
2013-09-16 15:40:56 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var nconf = require('nconf')
+  , express = require('express')
+  , sample_module  = require('sample_module')
+
+nconf.file('config.json', './config.json')
+
+var app = express();
+
+app.use(sample_module);
+
+app.get('/', function(req, res, next) {
+  console.log('This is the root. The foo is %s', nconf.get('foo'));
+  res.send(200);
+});
+
+app.listen(3000);
+
